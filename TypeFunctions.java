92693abf57ecba4f7137350f952beed5140f5889
Marcelo <gussanma@student.gu.se>
2022-09-21 14:27:00 +0200

    task 4 finished

--- TypeFunctions.java
+++ TypeFunctions.java
@@ -1,17 +1,57 @@
-
+/*Function that will retourn the damage multiplier when called in battle, based on the attacker and defender type */
 
 public class TypeFunctions {
-    Types type;
 
-    public TypeFunctions(Types type){
-        this.type= type;
-    }
+    public static double battleTypes(int attacker, int target) {
+        if (attacker == 0) {
+            switch (target) {
+                case (0):
+                    return 0.5;
 
-    public void battleTypes(){
-        int damage=
-        
+                case (1):
+                    return 0.5;
+
+                case (2):
+                    return 2.0;
+
+                default:
+                    return 1.0;
+            }
+                }
+        else if(attacker == 1){
+            switch (target) {
+                case (0):
+                    return 2.0;
+
+                case (1):
+                    return 0.5;
+
+                case (2):
+                    return 0.5;
+
+                default:
+                    return 1.0;
         }
-        
 
     }
+        else if(attacker == 2){
+            switch (target) {
+            case (0):
+                return 0.5;
 
+            case (1):
+                return 2.0;
+
+            case (2):
+                return 0.5;
+
+            default:
+                return 1.0;
+    }
+    
+
+}
+        else return 1.0;
+
+    }
+}
