343df0efe7dcf88b935f76c57269adc0e3b6aa90
Andrew Demchenko <demch.andrew@gmail.com>
2022-09-21 11:56:52 +0200

    created item class

---
+++ Item.java
@@ -0,0 +1,12 @@
+public class Item {
+    public final String itemName;
+    public final int healPwr;
+    public final double weight;
+                                        // default constructor
+    public Item(String itemName, int healPwr, double weight) {
+        this.itemName = itemName;
+        this.healPwr = healPwr;
+        this.weight = weight;
+    }
+
+}
