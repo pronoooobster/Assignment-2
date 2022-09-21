f6820c4ff18af134003d3dfd56967fc360b67167
Andrew Demchenko <demch.andrew@gmail.com>
2022-09-21 12:51:46 +0200

    finished items

--- Item.java
+++ Item.java
@@ -8,5 +8,20 @@ public class Item {
         this.healPwr = healPwr;
         this.weight = weight;
     }
+                                        // convert to string for printing
+    public String toString() {
+        double truncWeight;
+        truncWeight = weight;
+        truncWeight *= 100;
+        truncWeight = Math.floor(truncWeight);
+        truncWeight /= 100;
+        return itemName + " heals " + healPwr + " HP. (" + truncWeight + ")";
+    }
 
+    public void heal(Pokemon pokemon) {
+        pokemon.hp += healPwr;
+        if(pokemon.hp > pokemon.MAX_HP) {
+            pokemon.hp = pokemon.MAX_HP;
+        }
+    }
 }
